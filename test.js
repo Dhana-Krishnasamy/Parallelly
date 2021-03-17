1fd8a9cc8de80afcbb5b6cb46901ad97cfb75e82
Dhana <itsmedhana2004@yahoo.com>
2021-03-17 17:43:52 +0000

    Update test.js

--- test.js
+++ test.js
@@ -9,14 +9,16 @@ it(
   "parallelly should execute all the items in the queue",
   async done => {
     let count = 0;
+    //--------//
     await parallelly(
       async taskId => {
         count = count + taskId;
-        await wait();
+        await wait(); // Simulate time consuming work ( Waiting for network/file )
       },
-      4,
-      2
+      4, // Total tasks
+      2 // Number of tasks to run each time
     );
+    //--------//
     //0+1+2+3
     expect(count).toBe(6);
     done();
